3e279f3ed29f96c36e7669afae0fe20fde5c44a9
wesley <wesley@ecompleta.com.br>
2018-06-27 18:30:16 -0300

    First Commit

---
+++ src/main/java/com/wesleyfuchter/imageuploader/ImageUploaderApplication.java
@@ -0,0 +1,12 @@
+package com.wesleyfuchter.imageuploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageUploaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageUploaderApplication.class, args);
+    }
+}
